d76f2ed8300f8ea680599469c509eb182e717998
golenninjadragon <67926366+golenninjadragon@users.noreply.github.com>
2020-07-13 22:25:17 -0400

    Completed game 5 clues

--- Game5/js/main.js
+++ Game5/js/main.js
@@ -3,12 +3,98 @@
 /* eslint-env jquery */
 
 $(document).ready(function () {
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+
     Scrambler({
         target: '.scramble',
-        random: [750, 1500],
-        speed: 75,
+        random: [750, 2000],
+        speed: 100,
         afterAll: function () {
             $('.scramble').removeClass('scramble')
         }
     });
+
+    $('.menu-return').click(function () {
+        $('body').removeClass('bg-overlay');
+        $('header, #body').removeClass('hidden');
+        $('#code1').addClass('hidden');
+        $('#code2').addClass('hidden');
+        $('#code3').addClass('hidden');
+    });
+
+    $('#link1').click(function () {
+        $('body').addClass('bg-overlay');
+        $('header, #body').addClass('hidden');
+        $('#code1').removeClass('hidden');
+        $('#code2').addClass('hidden');
+        $('#code3').addClass('hidden');
+        Scrambler({
+            target: '.scramble1',
+            random: [60000, 60000],
+            speed: 200,
+        });
+    });
+
+    $('#link2').click(function () {
+        $('body').addClass('bg-overlay');
+        $('header, #body').addClass('hidden');
+        $('#code1').addClass('hidden');
+        $('#code2').removeClass('hidden');
+        $('#code3').addClass('hidden');
+
+        Scrambler({
+            target: '.scramble2',
+            random: [60000, 60000],
+            speed: 100,
+        });
+    });
+
+    $('#link3').click(function () {
+        $('body').addClass('bg-overlay');
+        $('header, #body').addClass('hidden');
+        $('#code1').addClass('hidden');
+        $('#code2').addClass('hidden');
+        $('#code3').removeClass('hidden');
+
+        Scrambler({
+            target: '.scramble3',
+            random: [60000, 60000],
+            speed: 100,
+        });
+    });
+    
+    if (getUrlParameter('unlock') == 'all') {
+        $('#link1').removeClass('locked');
+        $('#link2').removeClass('locked');
+        $('#link3').removeClass('locked');
+    } else if (getUrlParameter('unlock') !== undefined) {
+        $('#' + getUrlParameter('unlock')).removeClass('locked');
+    }
+
+    var currentDate = new Date();
+
+    if (currentDate >= new Date('7 August 2020 21:00:00 GMT-04:00')) {
+        $('#link1').removeClass('locked');
+    }
+
+    if (currentDate >= new Date('8 August 2020 21:00:00 GMT-04:00')) {
+        $('#link2').removeClass('locked');
+    }
+
+    if (currentDate >= new Date('9 August 2020 21:00:00 GMT-04:00')) {
+        $('#link3').removeClass('locked');
+    }
 });
